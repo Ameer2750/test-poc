b5fbc7fe52039e95199ca99941f646656bfc962b
Ameer <ameerbsc2630@gmail.com>
2025-07-17 14:56:34 +0530

    changed localhost to accept from anywhere

--- express-backend/main.js
+++ express-backend/main.js
@@ -75,6 +75,6 @@ app.post('/send', async (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
